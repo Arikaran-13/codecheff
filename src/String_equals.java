d250021369aadbe4e9c8b88ab9f8bf5aeff8fd5b
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-06-12 10:54:32 +0530

    by comparing both the string distinct char of b added to new string and hashset pplied

--- src/String_equals.java
+++ src/String_equals.java
@@ -1,5 +1,29 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class String_equals {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        int tc=sc.nextInt();
+        while(tc-->0){
+            ans();
+        }
+    }
+    public static void ans(){
+        int n = sc.nextInt();
+        sc.nextLine();
+        String a=sc.nextLine();
+        String b = sc.nextLine();
+String c ="";
+        for (int i = 0; i <n ; i++) {
+            if(a.charAt(i)!= b.charAt(i)){
+             c+=b.charAt(i);
+            }
+        }
+        HashSet<Character> hs = new HashSet<>();
+        for (int i = 0; i <c.length() ; i++) {
+            hs.add(c.charAt(i));
+        }
+        System.out.println(hs.size());
     }
 }
